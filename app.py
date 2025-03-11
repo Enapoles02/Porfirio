c6db6ab046091048f002db19fceda6b2d9e88dc4
Enapoles02 <enrique.napoles@dbschenker.com>
2025-03-11 16:28:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import json
 
-st.write(st.secrets.to_dict())
-
+try:
+    firebase_creds = json.loads(json.dumps(st.secrets["firebase_credentials"]))
+    st.write("✅ Firebase credentials loaded successfully!")
+except Exception as e:
+    st.error(f"❌ Error parsing Firebase credentials: {e}")
